cb0b0be44a0b22ec9a29fc5d7d3c0828ddae9a7e
Maham Babar <mahambabar11@gmail.com>
2024-01-06 06:13:32 -0800

    array

---
+++ Array/app.js
@@ -0,0 +1,20 @@
+const num = new Array (1,2,3,4,5,6,7,8,9);
+
+//push 
+num.push(10)
+
+//unshift
+num.unshift(0)
+
+//pop
+num.pop()
+
+//shift
+num.shift()
+
+// const names = [,]
+
+let names1 =["English","urdu","math"]
+let names2 = ["chem","phy","Bio"]
+let names3 =[...names1,...names2]
+console.log(names3);
